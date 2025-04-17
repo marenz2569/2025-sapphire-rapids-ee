b7b50d9bc168fec9f3525331228fe1beff8ec9d3
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-04-17 23:35:59 +0200

    fixes

--- measurements/shared-frequency-domains/test_idle.sh
+++ measurements/shared-frequency-domains/test_idle.sh
@@ -15,9 +15,10 @@ gcc while_true.c -o while_true
 
 elab ht disable
 
+# enable all cstates.
 echo 0 | sudo tee /sys/devices/system/cpu/cpu*/cpuidle/state*/disable
 
-#set low frequency on all CPUs
+# set low frequency on all CPUs
 echo userspace | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor > /dev/null
 
 # for all 56 CPUs
@@ -25,17 +26,21 @@ CPUS=`seq 0 55`
 
 for CPU1 in $CPUS; do
   for CPU2 in $CPUS; do
-  #set low frequency on all CPUs
+    if [[ $CPU1 -eq $CPU2 ]];
+    then
+      continue
+    fi
+    # set low frequency on all CPUs
     echo 800000 | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_setspeed > /dev/null
-  #set high frequency on the influential CPU
+    # set high frequency on the influential CPU
     echo 2400000 | sudo tee /sys/devices/system/cpu/cpu${CPU2}/cpufreq/scaling_setspeed > /dev/null
-  #explicitly set tested CPU to low frequency
+    # explicitly set tested CPU to low frequency
     echo 800000 | sudo tee /sys/devices/system/cpu/cpu${CPU1}/cpufreq/scaling_setspeed > /dev/null
-    #test frequency on tested CPU via perf
+    # test frequency on tested CPU via perf
     FREQ=`taskset -c ${CPU1} perf stat --log-fd 1 -e cycles -x ' ' timeout 1s ./while_true | grep -v "not counted" - | awk '{print $1}' `
-    #it should be 800 MHz
-    if [ $FREQ -gt 1400000000 ]; then
-      echo "$CPU2 influences $CPU1: $FREQ"
+    # it should be 800 MHz, test with 20% addition.
+    if [ $FREQ -gt (800000000 * 1.2) ]; then
+      echo "cpu $CPU2 influences cpu $CPU1: frequency of cpu $CPU1 is $FREQ Hz instead of 800MHz"
     fi
     sleep 1
   done
