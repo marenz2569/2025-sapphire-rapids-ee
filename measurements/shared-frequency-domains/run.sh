781f61f5bcaddadf70c982a0ab797a08e55950f4
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-04-17 21:44:24 +0000

    add run script

--- measurements/shared-frequency-domains/run.sh
+++ measurements/shared-frequency-domains/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 mkdir -p results
-OUTFILE="results/active_$(date +"%Y-%m-%d").datafile"
+ACTIVE_OUTFILE="results/active_$(date +"%Y-%m-%d").datafile"
+IDLE_OUTFILE="results/idle_$(date +"%Y-%m-%d").datafile"
 
-./test_active.sh > $OUTFILE
+./test_active.sh > $ACTIVE_OUTFILE
+./test_idle.sh > $IDLE_OUTFILE
