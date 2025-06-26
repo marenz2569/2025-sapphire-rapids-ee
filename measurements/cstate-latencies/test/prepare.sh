851e08c87b1d4e87d66ac91d837f704284a7536c
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-06-26 22:28:53 +0200

    cstate-latencies: isol second socket completely

--- measurements/cstate-latencies/test/prepare.sh
+++ measurements/cstate-latencies/test/prepare.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo elab reboot Ubuntu-6.8.0-60-generic -a "isolcpus=1,2,3,57,58 nohz_full=1,2,3,57,58"
+sudo elab reboot Ubuntu-6.8.0-60-generic -a "isolcpus=1,2,3,56-111,168-223 nohz_full=1,2,3,56-111,168-223"
