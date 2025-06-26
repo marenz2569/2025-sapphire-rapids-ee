6b246c9e0f23df569d7bbd1686b2e0a88e94a4ab
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-06-26 20:54:58 +0000

    cstate-latencies: only measure C6 and rerun on hati, changed measurement settings

--- measurements/cstate-latencies/test/prepare.sh
+++ measurements/cstate-latencies/test/prepare.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo elab reboot Ubuntu-6.8.0-60-generic -a "isolcpus=1,2,3,56-111,168-223 nohz_full=1,2,3,56-111,168-223"
+elab reboot Ubuntu-6.8.0-60-generic -a "isolcpus=1,2,3,56-111,168-223 nohz_full=1,2,3,56-111,168-223"
