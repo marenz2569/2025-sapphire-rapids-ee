0e77dd50cc42df1b5c733095f75284dca2ed5e1c
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-08-05 20:16:00 +0200

    shared-frequency-domains: fix call of other scripts

--- measurements/software/shared-frequency-domains/run.sh
+++ measurements/software/shared-frequency-domains/run.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+# Traverse to the path of this script
+# https://stackoverflow.com/a/24112741
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
 ./test_active.sh > $RESULTS_FOLDER/active.log
 ./test_idle.sh > $RESULTS_FOLDER/idle.log
