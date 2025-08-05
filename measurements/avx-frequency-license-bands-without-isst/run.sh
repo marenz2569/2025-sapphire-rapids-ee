7a6ae6e07d947ef2bf3d02de5ef87c76474dc635
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-08-05 12:45:48 +0200

    avx-frequency-license-bands-without-isst: make timeouts of firestarter configurable. use measurement furation of 60 seconds, discarding the first and last 5 seconds

--- measurements/avx-frequency-license-bands-without-isst/run.sh
+++ measurements/avx-frequency-license-bands-without-isst/run.sh
@@ -52,6 +52,11 @@ uncore_frequencies=(0x0808 0x1010 0x1919 0x0819)
 # python3 -c 'print(" ".join([ str(14*i + j) for j in range(14) for i in range(4) ]))'
 alt_order=(0 14 28 42 1 15 29 43 2 16 30 44 3 17 31 45 4 18 32 46 5 19 33 47 6 20 34 48 7 21 35 49 8 22 36 50 9 23 37 51 10 24 38 52 11 25 39 53 12 26 40 54 13 27 41 55)
 
+# Run each firestarter measurement for 60 seconds and cut away the first and last 5 seconds of the measurement data
+START_DELTA=5000
+STOP_DELTA=5000
+TIMEOUT=60
+
 # Measurement loop. Run all experiments with all number of cores on socket 0.
 for core_frequency in "${core_frequencies[@]}"; do
     for uncore_frequency in "${uncore_frequencies[@]}"; do
@@ -62,7 +67,6 @@ for core_frequency in "${core_frequencies[@]}"; do
 
         for ((i = 0 ; i < 56 ; i++)); do
             echo "Running with $i cores."
-            # Measure 10 seconds, discarding the first 5 and last 2 seconds
             if [[ $i -eq 0 ]];
             then
                 ROWS_BINDLIST=0
@@ -73,15 +77,15 @@ for core_frequency in "${core_frequencies[@]}"; do
             fi
 
             # we need to access /sys/class/powercap
-            sudo $FIRESTARTER -b $ROWS_BINDLIST --measurement --start-delta=5000 --start-delta=2000 -t 10 -i 6 --run-instruction-groups=REG:100  | tail -n 9 > $OUTFOLDER/$core_frequency/$uncore_frequency/rows/lic0/$i.csv
-            sudo $FIRESTARTER -b $ROWS_BINDLIST --measurement --start-delta=5000 --start-delta=2000 -t 10 -i 6 --run-instruction-groups=REG:100,L1_L:100 | tail -n 9 > $OUTFOLDER/$core_frequency/$uncore_frequency/rows/lic1/$i.csv
-            sudo $FIRESTARTER -b $ROWS_BINDLIST --measurement --start-delta=5000 --start-delta=2000 -t 10 --run-instruction-groups=REG:100 | tail -n 9 > $OUTFOLDER/$core_frequency/$uncore_frequency/rows/lic2/$i.csv
-            sudo $FIRESTARTER -b $ROWS_BINDLIST --measurement --start-delta=5000 --start-delta=2000 -t 10 --run-instruction-groups=L3_L:100 | tail -n 9 > $OUTFOLDER/$core_frequency/$uncore_frequency/rows/lic3/$i.csv
+            sudo $FIRESTARTER -b $ROWS_BINDLIST --measurement --start-delta=$START_DELTA --start-delta=$STOP_DELTA -t $TIMEOUT -i 6 --run-instruction-groups=REG:100  | tail -n 9 > $OUTFOLDER/$core_frequency/$uncore_frequency/rows/lic0/$i.csv
+            sudo $FIRESTARTER -b $ROWS_BINDLIST --measurement --start-delta=$START_DELTA --start-delta=$STOP_DELTA -t $TIMEOUT -i 6 --run-instruction-groups=REG:100,L1_L:100 | tail -n 9 > $OUTFOLDER/$core_frequency/$uncore_frequency/rows/lic1/$i.csv
+            sudo $FIRESTARTER -b $ROWS_BINDLIST --measurement --start-delta=$START_DELTA --start-delta=$STOP_DELTA -t $TIMEOUT --run-instruction-groups=REG:100 | tail -n 9 > $OUTFOLDER/$core_frequency/$uncore_frequency/rows/lic2/$i.csv
+            sudo $FIRESTARTER -b $ROWS_BINDLIST --measurement --start-delta=$START_DELTA --start-delta=$STOP_DELTA -t $TIMEOUT --run-instruction-groups=L3_L:100 | tail -n 9 > $OUTFOLDER/$core_frequency/$uncore_frequency/rows/lic3/$i.csv
 
-            sudo $FIRESTARTER -b $COLUMNS_BINDLIST --measurement --start-delta=5000 --start-delta=2000 -t 10 -i 6 --run-instruction-groups=REG:100  | tail -n 9 > $OUTFOLDER/$core_frequency/$uncore_frequency/columns/lic0/$i.csv
-            sudo $FIRESTARTER -b $COLUMNS_BINDLIST --measurement --start-delta=5000 --start-delta=2000 -t 10 -i 6 --run-instruction-groups=REG:100,L1_L:100 | tail -n 9 > $OUTFOLDER/$core_frequency/$uncore_frequency/columns/lic1/$i.csv
-            sudo $FIRESTARTER -b $COLUMNS_BINDLIST --measurement --start-delta=5000 --start-delta=2000 -t 10 --run-instruction-groups=REG:100 | tail -n 9 > $OUTFOLDER/$core_frequency/$uncore_frequency/columns/lic2/$i.csv
-            sudo $FIRESTARTER -b $COLUMNS_BINDLIST --measurement --start-delta=5000 --start-delta=2000 -t 10 --run-instruction-groups=L3_L:100 | tail -n 9 > $OUTFOLDER/$core_frequency/$uncore_frequency/columns/lic3/$i.csv
+            sudo $FIRESTARTER -b $COLUMNS_BINDLIST --measurement --start-delta=$START_DELTA --start-delta=$STOP_DELTA -t $TIMEOUT -i 6 --run-instruction-groups=REG:100  | tail -n 9 > $OUTFOLDER/$core_frequency/$uncore_frequency/columns/lic0/$i.csv
+            sudo $FIRESTARTER -b $COLUMNS_BINDLIST --measurement --start-delta=$START_DELTA --start-delta=$STOP_DELTA -t $TIMEOUT -i 6 --run-instruction-groups=REG:100,L1_L:100 | tail -n 9 > $OUTFOLDER/$core_frequency/$uncore_frequency/columns/lic1/$i.csv
+            sudo $FIRESTARTER -b $COLUMNS_BINDLIST --measurement --start-delta=$START_DELTA --start-delta=$STOP_DELTA -t $TIMEOUT --run-instruction-groups=REG:100 | tail -n 9 > $OUTFOLDER/$core_frequency/$uncore_frequency/columns/lic2/$i.csv
+            sudo $FIRESTARTER -b $COLUMNS_BINDLIST --measurement --start-delta=$START_DELTA --start-delta=$STOP_DELTA -t $TIMEOUT --run-instruction-groups=L3_L:100 | tail -n 9 > $OUTFOLDER/$core_frequency/$uncore_frequency/columns/lic3/$i.csv
         done
     done
 done
