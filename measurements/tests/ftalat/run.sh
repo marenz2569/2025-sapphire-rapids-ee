d5905ac4f2f1a8ad07cf0a1a10bbf08d41a4a77c
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-08-06 13:48:23 +0200

    move ftalat measurement

---
+++ measurements/tests/ftalat/run.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# Test if the processor supports scaling_available_frequencies
+# If it does, we will use the userspace governour and write to scaling_setspeed
+# if not, we use the performance governor and write to scaling_max_speed
+$(test -e /sys/bus/cpu/devices/cpu0/cpufreq/scaling_available_frequencies)
+scaling_available_frequencies_found=$?
+
+if [ $scaling_available_frequencies_found -eq 0 ]
+then
+	frequencies=(`cat /sys/bus/cpu/devices/cpu0/cpufreq/scaling_available_frequencies`)
+
+	echo userspace | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_governor
+	# Set all threads to the lowest frequency
+	cat /sys/bus/cpu/devices/cpu0/cpufreq/scaling_min_freq | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_setspeed
+else
+	# loop over to scaling_min_frequency to scaling_max_frequency in 100kHz steps
+	frequencies=()
+	min_frequency=`cat /sys/bus/cpu/devices/cpu0/cpufreq/scaling_min_freq`
+	max_frequency=`cat /sys/bus/cpu/devices/cpu0/cpufreq/scaling_max_freq`
+    for ((i = min_frequency ; i <= max_frequency ; i+=100000)); do
+		frequencies+=($i)
+	done
+
+	echo performance | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_governor
+	# Set all threads to the lowest frequency
+	cat /sys/bus/cpu/devices/cpu0/cpufreq/scaling_min_freq | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_max_freq
+fi
+
+echo "Running ftalat for following frequencies:"
+printf '%s ' "${frequencies[@]}"
+echo ""
+
+# Disable cstates
+echo 1 | sudo tee /sys/devices/system/cpu/cpu*/cpuidle/state*/disable
+
+# Take hyperthreads offline
+echo off | sudo tee /sys/devices/system/cpu/smt/control
+
+if [ $scaling_available_frequencies_found -eq 0 ]
+then
+	FTALAT=$FTALAT_SCALING_SETSPEED
+else
+	FTALAT=$FTALAT_SCALING_MAX_FREQ
+fi
+
+for START in "${frequencies[@]}"
+do
+	for TARGET in "${frequencies[@]}"
+	do
+		if [ $START -eq $TARGET ]
+		then
+			echo "Skipping $START -> $TARGET (same frequency)"
+			continue
+		fi
+		echo "Running $START -> $TARGET"
+		sudo $FTALAT $START $TARGET > $RESULTS_FOLDER/${START}_${TARGET}.txt
+	done
+done
