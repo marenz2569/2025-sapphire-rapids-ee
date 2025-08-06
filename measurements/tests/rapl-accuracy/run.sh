cb5dbe98787e45ed01526c9d4275e32230224e1d
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-08-06 13:22:56 +0200

    move rapl-accuracy to tests

---
+++ measurements/tests/rapl-accuracy/run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# TODO: add build for this module
+module load firestarter-metric-metricq
+
+echo off | sudo tee /sys/devices/system/cpu/smt/control
+
+export GOMP_CPU_AFFINITY=0-111
+export METRICQ_METRICS=elab.hati.pdu.power
+
+echo "environment variables:"
+echo "  GOMP_CPU_AFFINITY = $GOMP_CPU_AFFINITY"
+echo "  METRICQ_METRICS   = $METRICQ_METRICS"
+echo "executing test..."
+
+$ROCO2 --csv-output $RESULTS_FOLDER/results.csv
