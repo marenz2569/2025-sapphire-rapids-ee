5b5716bed023ad65f2e41494171e6f7e0d1d2c53
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-08-11 18:54:00 +0200

    tests/dump-msr: dump msr space up to 0xffff

--- measurements/tests/dump-msr/dump-msr.sh
+++ measurements/tests/dump-msr/dump-msr.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# Read the Msr space from 0..0xffffffff
-for ((i = 0 ; i < 4294967296 ; i++)); do
+# Read the Msr space from 0..0xffff
+for ((i = 0 ; i < 65535 ; i++)); do
     echo -n "$i: "
     rdmsr -p 0 $i || echo ""
 done
