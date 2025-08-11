6ffe354c1e24b372e5036ec43512b2c707b9c923
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-08-11 22:42:28 +0200

    Revert "set stream array size to 100000000"
    This reverts commit b6ff23e059a589ae3c1a3ac5aeda73e92e599e4f.

--- measurements/software/InstallStream.cmake
+++ measurements/software/InstallStream.cmake
@@ -9,7 +9,7 @@ ExternalProject_Add(
     INSTALL_DIR       "${CMAKE_CURRENT_BINARY_DIR}/stream-install"
 	BUILD_IN_SOURCE 1
 	CONFIGURE_COMMAND ""
-	BUILD_COMMAND     gcc <SOURCE_DIR>.c -o stream -DSTREAM_ARRAY_SIZE=100000000
+	BUILD_COMMAND     gcc <SOURCE_DIR>.c -o stream
 	INSTALL_COMMAND   mkdir -p <INSTALL_DIR> && cp <SOURCE_DIR>/stream <INSTALL_DIR>/stream
 	TEST_COMMAND      ""
 )
