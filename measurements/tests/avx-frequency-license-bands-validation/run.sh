897c9202c662a28244b5783683fa29a0f9043922
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-08-12 13:37:10 +0200

    tests/avx-frequency-license-bands-*: remove alternate bindlist as this has no measureable effect on the processor

--- measurements/tests/avx-frequency-license-bands-validation/run.sh
+++ measurements/tests/avx-frequency-license-bands-validation/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir -p $RESULTS_FOLDER/{normal,alt}/{lic0,lic1,lic2,lic3} || true
+mkdir -p $RESULTS_FOLDER/{lic0,lic1,lic2,lic3} || true
 
 echo off | sudo tee /sys/devices/system/cpu/smt/control
 
@@ -18,32 +18,19 @@ START_DELTA=30000
 STOP_DELTA=5000
 TIMEOUT=60
 
-# column per column on socket 0
-alt_order=(0 4 7 11 14 18 21 24 28 32 35 39 42 46 50 53 1 5 8 12 15 19 22 25 29 33 36 40 43 47 51 54 2 6 9 13 16 20 23 26 30 34 37 41 44 48 52 55 3 10 17 27 31 38 45 49)
-
 # Measurement loop. Run all experiments with all number of cores on socket 0.
 for ((i = 0 ; i < 56 ; i++)); do
     echo "Running with $i cores."
     if [[ $i -eq 0 ]];
     then
         BINDLIST=0
-        ALT_BINDLIST="${alt_order[i]}"
     else
         BINDLIST=0-$i
-        ALT_BINDLIST="$ALT_BINDLIST,${alt_order[i]}"
     fi
 
     # sudo, as we need to access /sys/class/powercap
-
-    # Normal bindlist
-    sudo -E $FIRESTARTER -b $BINDLIST     --measurement --start-delta=$START_DELTA --stop-delta=$STOP_DELTA -t $TIMEOUT -i 6 --run-instruction-groups=REG:100          | tail -n 9 > $RESULTS_FOLDER/normal/lic0/$i.csv
-    sudo -E $FIRESTARTER -b $BINDLIST     --measurement --start-delta=$START_DELTA --stop-delta=$STOP_DELTA -t $TIMEOUT -i 6 --run-instruction-groups=REG:100,L1_L:100 | tail -n 9 > $RESULTS_FOLDER/normal/lic1/$i.csv
-    sudo -E $FIRESTARTER -b $BINDLIST     --measurement --start-delta=$START_DELTA --stop-delta=$STOP_DELTA -t $TIMEOUT      --run-instruction-groups=REG:100          | tail -n 9 > $RESULTS_FOLDER/normal/lic2/$i.csv
-    sudo -E $FIRESTARTER -b $BINDLIST     --measurement --start-delta=$START_DELTA --stop-delta=$STOP_DELTA -t $TIMEOUT      --run-instruction-groups=L3_L:100         | tail -n 9 > $RESULTS_FOLDER/normal/lic3/$i.csv
-
-    # Alternating bindlist
-    sudo -E $FIRESTARTER -b $ALT_BINDLIST --measurement --start-delta=$START_DELTA --stop-delta=$STOP_DELTA -t $TIMEOUT -i 6 --run-instruction-groups=REG:100          | tail -n 9 > $RESULTS_FOLDER/alt/lic0/$i.csv
-    sudo -E $FIRESTARTER -b $ALT_BINDLIST --measurement --start-delta=$START_DELTA --stop-delta=$STOP_DELTA -t $TIMEOUT -i 6 --run-instruction-groups=REG:100,L1_L:100 | tail -n 9 > $RESULTS_FOLDER/alt/lic1/$i.csv
-    sudo -E $FIRESTARTER -b $ALT_BINDLIST --measurement --start-delta=$START_DELTA --stop-delta=$STOP_DELTA -t $TIMEOUT      --run-instruction-groups=REG:100          | tail -n 9 > $RESULTS_FOLDER/alt/lic2/$i.csv
-    sudo -E $FIRESTARTER -b $ALT_BINDLIST --measurement --start-delta=$START_DELTA --stop-delta=$STOP_DELTA -t $TIMEOUT      --run-instruction-groups=L3_L:100         | tail -n 9 > $RESULTS_FOLDER/alt/lic3/$i.csv
+    sudo -E $FIRESTARTER -b $BINDLIST     --measurement --start-delta=$START_DELTA --stop-delta=$STOP_DELTA -t $TIMEOUT -i 6 --run-instruction-groups=REG:100          | tail -n 9 > $RESULTS_FOLDER/lic0/$i.csv
+    sudo -E $FIRESTARTER -b $BINDLIST     --measurement --start-delta=$START_DELTA --stop-delta=$STOP_DELTA -t $TIMEOUT -i 6 --run-instruction-groups=REG:100,L1_L:100 | tail -n 9 > $RESULTS_FOLDER/lic1/$i.csv
+    sudo -E $FIRESTARTER -b $BINDLIST     --measurement --start-delta=$START_DELTA --stop-delta=$STOP_DELTA -t $TIMEOUT      --run-instruction-groups=REG:100          | tail -n 9 > $RESULTS_FOLDER/lic2/$i.csv
+    sudo -E $FIRESTARTER -b $BINDLIST     --measurement --start-delta=$START_DELTA --stop-delta=$STOP_DELTA -t $TIMEOUT      --run-instruction-groups=L3_L:100         | tail -n 9 > $RESULTS_FOLDER/lic3/$i.csv
 done
