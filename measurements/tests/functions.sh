d86f54d8eba84c9e3976f5457ed682dbf7fcfbca
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-08-15 18:23:19 +0200

    tests: set uncore and core frequencies based on hostname

--- measurements/tests/functions.sh
+++ measurements/tests/functions.sh
@@ -3,6 +3,8 @@
 # Reset the the SMT control, cpu frequency and intel-speed-select back to defaults.
 # The first argument specifes which cpu governor should be applied
 function reset_cpu_controls() {
+    local hostname=$(hostname)
+
     # Switch all cpus on
     echo on | sudo tee /sys/devices/system/cpu/smt/control
 
@@ -12,12 +14,30 @@ function reset_cpu_controls() {
     # Use specified governor
     echo $1 | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 
-    # Restore full cpu frequency range
-    echo 800000 | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_min_freq
-    echo 3800000 | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_max_freq
+    # Enable RAPL readout
+    lsmod | grep -w intel_rapl_msr && echo "intel_rapl_msr already loaded" || { echo "Loading intel_rapl_msr module"; sudo modprobe intel_rapl_msr ; }
 
-    # Restore uncore frequency range
-    sudo wrmsr 0x620 0x0819
+    # Enable MSR readout
+    lsmod | grep -w msr && echo "MSR already loaded" || { echo "Loading MSR module"; sudo modprobe msr ; }
+
+    if [[ $hostname -eq "hati" ]]; then
+        # Restore full cpu frequency range
+        echo 800000 | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_min_freq
+        echo 3800000 | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_max_freq
+
+        # Restore uncore frequency range
+        sudo wrmsr -a 0x620 0x0819
+    else if [[ $hostname -eq "ariel" ]]; then
+        # Restore full cpu frequency range
+        echo 1200000 | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_min_freq
+        echo 3001000 | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_max_freq
+
+        # Restore uncore frequency range
+        sudo wrmsr -a 0x620 0x0c18
+    else
+        echo "Host $hostname is not supported by the reset_cpu_controls function"
+        exit 1
+    fi
 
     # Test if kernel supports the ISST interface
     $(test -e /dev/isst)
@@ -30,10 +50,4 @@ function reset_cpu_controls() {
         sudo $ISST turbo-freq disable
         sudo $ISST core-power disable
     fi
-
-    # Enable RAPL readout
-    lsmod | grep -w intel_rapl_msr && echo "intel_rapl_msr already loaded" || { echo "Loading intel_rapl_msr module"; sudo modprobe intel_rapl_msr ; }
-
-    # Enable MSR readout
-    lsmod | grep -w msr && echo "MSR already loaded" || { echo "Loading MSR module"; sudo modprobe msr ; }
 }
