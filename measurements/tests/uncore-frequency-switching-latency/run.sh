3a1da7da622a893c21d6464ce5ee3eca8ddbeb8f
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-08-16 00:13:58 +0200

    tests/uncore-frequency-switching-latency: use stdbuf -oL to make sure we flush every line

--- measurements/tests/uncore-frequency-switching-latency/run.sh
+++ measurements/tests/uncore-frequency-switching-latency/run.sh
@@ -2,15 +2,15 @@
 
 # This test needs to run on CPU 0, as the code for the UFS latency measurement reads and writes the MSR on this specific CPU.
 
-sudo taskset -c 0 $UFS_MANUAL > $RESULTS_FOLDER/manual.performance.out
-sudo taskset -c 0 $UFS_AUTOMATIC > $RESULTS_FOLDER/automatic.performance.out
+sudo taskset -c 0 stdbuf -oL $UFS_MANUAL > $RESULTS_FOLDER/manual.performance.out
+sudo taskset -c 0 stdbuf -oL $UFS_AUTOMATIC > $RESULTS_FOLDER/automatic.performance.out
 
 echo 2200000 | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_min_freq
 echo 2200000 | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_max_freq
 
-sudo taskset -c 0 $UFS_MANUAL > $RESULTS_FOLDER/manual.2200.out
+sudo taskset -c 0 stdbuf -oL $UFS_MANUAL > $RESULTS_FOLDER/manual.2200.out
 
 echo 800000 | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_min_freq
 echo 800000 | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_max_freq
 
-sudo taskset -c 0 $UFS_MANUAL > $RESULTS_FOLDER/manual.800.out
+sudo taskset -c 0 stdbuf -oL $UFS_MANUAL > $RESULTS_FOLDER/manual.800.out
