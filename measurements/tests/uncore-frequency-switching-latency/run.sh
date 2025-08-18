750d7dcbfd28ce58c41fb2079b987208a525a065
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-08-19 01:12:26 +0200

    tests/uncore-frequency-switching-latency: do not run automatic test

--- measurements/tests/uncore-frequency-switching-latency/run.sh
+++ measurements/tests/uncore-frequency-switching-latency/run.sh
@@ -3,7 +3,7 @@
 # This test needs to run on CPU 0, as the code for the UFS latency measurement reads and writes the MSR on this specific CPU.
 
 sudo taskset -c 0 stdbuf -oL $UFS_MANUAL > $RESULTS_FOLDER/manual.performance.out
-sudo taskset -c 0 stdbuf -oL $UFS_AUTOMATIC > $RESULTS_FOLDER/automatic.performance.out
+# sudo taskset -c 0 stdbuf -oL $UFS_AUTOMATIC > $RESULTS_FOLDER/automatic.performance.out
 
 echo 2200000 | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_min_freq
 echo 2200000 | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_max_freq
