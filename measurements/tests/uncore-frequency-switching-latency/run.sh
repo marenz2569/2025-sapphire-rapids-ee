3aa6c9ca278e088b7fc6c8ee693b63581e33f2a4
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-08-19 11:12:05 +0200

    tests/uncore-frequency-switching-latency: run at 800MHz and nomimal frequency

--- measurements/tests/uncore-frequency-switching-latency/run.sh
+++ measurements/tests/uncore-frequency-switching-latency/run.sh
@@ -2,14 +2,16 @@
 
 # This test needs to run on CPU 0, as the code for the UFS latency measurement reads and writes the MSR on this specific CPU.
 
-sudo taskset -c 0 stdbuf -oL $UFS_MANUAL > $RESULTS_FOLDER/manual.performance.out
+# sudo taskset -c 0 stdbuf -oL $UFS_MANUAL > $RESULTS_FOLDER/manual.performance.out
 # sudo taskset -c 0 stdbuf -oL $UFS_AUTOMATIC > $RESULTS_FOLDER/automatic.performance.out
 
-echo 2200000 | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_min_freq
-echo 2200000 | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_max_freq
+# Run at nominal frequency
+echo 2000000 | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_min_freq
+echo 2000000 | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_max_freq
 
 sudo taskset -c 0 stdbuf -oL $UFS_MANUAL > $RESULTS_FOLDER/manual.2200.out
 
+# Run at lowest frequency
 echo 800000 | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_min_freq
 echo 800000 | sudo tee /sys/bus/cpu/devices/cpu*/cpufreq/scaling_max_freq
 
