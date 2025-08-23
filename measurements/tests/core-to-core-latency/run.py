e614c1ec057d4676b6a5500b596646a69fb26ebd
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-08-23 18:08:50 +0200

    tests/core-to-core-latency: run the measurement an save the results

--- measurements/tests/core-to-core-latency/run.py
+++ measurements/tests/core-to-core-latency/run.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import json
+import os
 import subprocess
 from utils.numactl_parser import NumaNode, NumaNodes
 from utils.lscpu_parser import LscpuInformation
 from typing import List
 import glob
+from pathlib import Path
+import shutil
 
 def get_all_cpus(nodes: List[NumaNode]) -> List[int]:
     cpus = list()
@@ -32,7 +36,7 @@ def set_uncore_frequency(frequency_in_100mhz: int):
     uncore_frequency_string = hex(frequency_in_100mhz << 8 | frequency_in_100mhz)
     subprocess.run(['sudo', 'wrmsr', '-a', '0x620', uncore_frequency_string], capture_output=True)
 
-def measure(nodes: List[NumaNode]):
+def measure(nodes: List[NumaNode], settings: dict, results_folder: Path):
     all_cpus = get_all_cpus(nodes)
 
     # We loop over all numa nodes and measure the cache line latencies from
@@ -41,14 +45,65 @@ def measure(nodes: List[NumaNode]):
     for node in nodes:
         for this_cpu in node.cpu_list:
             for other_cpu in all_cpus:
+                # Skip the self edge
+                if this_cpu == other_cpu:
+                    continue
+
+                # For two cpus in the same NUMA node, we do not need to measure twice
+                if other_cpu in node.cpu_list and other_cpu > this_cpu:
+                    continue
+
                 # Create the directory for the measurement results
+                outfolder = results_folder / f'{this_cpu}' / f'{other_cpu}'
+                os.makedirs(outfolder)
+
+                settings['memory_in_numa_node'] = node.node_id
+                settings['this_cpu'] = this_cpu
+                settings['other_cpu'] = other_cpu
 
                 # This will run around 10 seconds on the same socket
-                # Run it in the created directory
-                print(f'numactl --membind={node.node_id} ./software/atomic-latencies/atomic-latencies {this_cpu},{other_cpu} 100 1000 64 100')
+                subprocess.run(
+                    ['numactl',
+                     f'--membind={node.node_id}',
+                     './software/atomic-latencies/atomic-latencies',
+                     # do a ping pong between the cores this_cpu and other_cpu
+                     f'{this_cpu},{other_cpu}',
+                     # repeat each pingpong for 100 times
+                     '100',
+                     # use 1000 caches lines
+                     '1000',
+                     # each with a size of 64B
+                     '64',
+                     # repeat the measurement for 100 times
+                     '100'],
+                    # Run it in the created directory. This will save flush_results.txt and latency_results.txt
+                    cwd=outfolder.absolute,
+                    capture_output=True,
+                    text=True)
 
+                with open(Path(outfolder / 'settings.json').absolute, 'w') as f:
+                    json.dump(settings, f)
+
+                # Duplicate the entries if we are in the same NUMA node
+                if other_cpu in node.cpu_list:
+                    settings['this_cpu'] = other_cpu
+                    settings['other_cpu'] = this_cpu
+
+                    copied_outfolder = results_folder / f'{other_cpu}' / f'{this_cpu}'
+                    os.makedirs(copied_outfolder)
+
+                    # Copy the results folder content
+                    shutil.copy(Path(outfolder / 'flush_results.txt').absolute, copied_outfolder)
+                    shutil.copy(Path(outfolder / 'latency_results.txt').absolute, copied_outfolder)
+                    with open(Path(copied_outfolder / 'settings.json').absolute, 'w') as f:
+                        json.dump(settings, f)
 
 def main():
+    if 'RESULTS_FOLDER' not in os.environ:
+        print('RESULTS_FOLDER env variable is not set')
+        exit(1)
+    results_foler = Path(os.environ['RESULTS_FOLDER'])
+
     disable_smt()
 
     nodes = NumaNodes.get_numa_nodes()
@@ -58,13 +113,25 @@ def main():
     # uncore frequencies in 100MHz
     uncore_frequencies = [ 8, 25 ]
 
+    # The settings of the current measurement
+    settings = dict()
+
     for core_frequency in core_frequencies:
         # Set the core frequency fixed
         set_core_frequency(core_frequency)
+        settings['core_frequency'] = core_frequency
+
+        # Folder for the measurements
+        core_folder = results_foler / f'{core_frequency}'
 
         for uncore_frequency in uncore_frequencies:
             # Set the uncore frequency fixed
             set_uncore_frequency(uncore_frequency)
+            settings['uncore_frequency'] = uncore_frequency
+
+            # Create folder for the measurements
+            uncore_folder = core_folder / f'{uncore_frequency}'
+            os.makedirs(uncore_folder.absolute)
 
             # Only run the measurement on socket 0
             # Assume that each socket has the same number of NUMA nodes
@@ -73,7 +140,7 @@ def main():
             print(f'Running measurement (core frequency: {core_frequency}kHz, uncore frequency: {uncore_frequency}00MHz) on nodes {filtered_nodes}')
 
             # Measure the cachelines of the node
-            measure(filtered_nodes)
+            measure(filtered_nodes, settings, uncore_folder)
 
 if __name__ == "__main__":
     main()
