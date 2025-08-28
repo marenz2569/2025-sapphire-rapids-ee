385624e12db8c6e6699bf49a5638842894964d37
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-08-28 12:14:36 +0200

    move isst_perf_profile_parser into experiment_utils

--- experiment_utils/experiment_utils/__init__.py
+++ experiment_utils/experiment_utils/__init__.py
@@ -1,2 +1,3 @@
 from .experiments import Experiment, ExperimentFilter # pyright: ignore[reportUnusedImport]
+from .isst_perf_profile_parser import TurboRatioLevel, Profile, IsstPerfProfile  # pyright: ignore[reportUnusedImport]
 from .plotting import Plotting # pyright: ignore[reportUnusedImport]
--- measurements/tests/avx-frequency-license-bands/parser/__init__.py
+++
@@ -1,3 +0,0 @@
-# pylint: disable=missing-module-docstring, deprecated-module
-
-from .isst_perf_profile_parser import TurboRatioLevel, Profile, IsstPerfProfile
