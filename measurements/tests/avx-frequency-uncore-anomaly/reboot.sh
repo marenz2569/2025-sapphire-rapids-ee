86297d76e06ed2c4a488d1ca77c087bdf45a6cf5
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-08-28 17:51:01 +0200

    use latest kernel version for the reboot

--- measurements/tests/avx-frequency-uncore-anomaly/reboot.sh
+++ measurements/tests/avx-frequency-uncore-anomaly/reboot.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # cores 0-8 isolated
-elab reboot Ubuntu-6.8.0-60-generic -a "isolcpus=0-8 nohz_full=0-8"
+elab reboot Ubuntu-6.8.0-65-generic -a "isolcpus=0-8 nohz_full=0-8"
--- measurements/tests/cstate-latencies/reboot.sh
+++ measurements/tests/cstate-latencies/reboot.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-elab reboot Ubuntu-6.8.0-60-generic -a "isolcpus=1,2,3,56-111,168-223 nohz_full=1,2,3,56-111,168-223"
+elab reboot Ubuntu-6.8.0-65-generic -a "isolcpus=1,2,3,56-111,168-223 nohz_full=1,2,3,56-111,168-223"
