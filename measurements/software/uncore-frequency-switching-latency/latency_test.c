581020950a7d76a303ba5cb6545262e5eef479af
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-08-30 22:31:05 +0200

    uncore-frequency-switching-latency: update license note

--- measurements/software/uncore-frequency-switching-latency/latency_test.c
+++ measurements/software/uncore-frequency-switching-latency/latency_test.c
@@ -1,19 +1,24 @@
-/*   Test program that triggers uncore frequency switches
+/*
+ * Test program that triggers uncore frequency switches
  *
- *    Copyright (C) 2019  TU Dresden
+ * Copyright (C) 2019 TU Dresden, Center for Information Services and
+ * High Performance Computing
+ * Copyright (C) 2025 Markus Schmidl
  *
- *    This program is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *    You should have received a copy of the GNU General Public License
- *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/\>.
  */
+
 #define _GNU_SOURCE 1
 
 #include <asm/unistd.h>
