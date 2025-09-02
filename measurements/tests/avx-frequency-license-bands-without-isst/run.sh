4bc7f7662acc10e93a91ecb6d1691850c2f7a90c
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-09-02 13:22:04 +0200

    avx-frequency-license-bands-without-isst: collect uncore frequency information

--- measurements/tests/avx-frequency-license-bands-without-isst/run.sh
+++ measurements/tests/avx-frequency-license-bands-without-isst/run.sh
@@ -7,6 +7,8 @@ uncore_frequencies=(0x0808 0x1010 0x1919 0x0819)
 START_DELTA=5000
 STOP_DELTA=5000
 TIMEOUT=60
+IUFD_TIMEOUT=$(echo "$TIMEOUT * 1000" | bc)
+UNCORE_READER_CORE=0
 
 # Measurement loop. Run all experiments with all number of cores on socket 0.
 for core_frequency in "${core_frequencies[@]}"; do
@@ -26,10 +28,21 @@ for core_frequency in "${core_frequencies[@]}"; do
             fi
 
             # we need to access /sys/class/powercap
-            sudo -E $FIRESTARTER -b $BINDLIST --measurement --start-delta=$START_DELTA --start-delta=$STOP_DELTA -t $TIMEOUT -i 6 --run-instruction-groups=REG:100  | tail -n 9 > $RESULTS_FOLDER/$core_frequency/$uncore_frequency/lic0/$i.csv
-            sudo -E $FIRESTARTER -b $BINDLIST --measurement --start-delta=$START_DELTA --start-delta=$STOP_DELTA -t $TIMEOUT -i 6 --run-instruction-groups=REG:100,L1_L:100 | tail -n 9 > $RESULTS_FOLDER/$core_frequency/$uncore_frequency/lic1/$i.csv
-            sudo -E $FIRESTARTER -b $BINDLIST --measurement --start-delta=$START_DELTA --start-delta=$STOP_DELTA -t $TIMEOUT --run-instruction-groups=REG:100 | tail -n 9 > $RESULTS_FOLDER/$core_frequency/$uncore_frequency/lic2/$i.csv
-            sudo -E $FIRESTARTER -b $BINDLIST --measurement --start-delta=$START_DELTA --start-delta=$STOP_DELTA -t $TIMEOUT --run-instruction-groups=L3_L:100 | tail -n 9 > $RESULTS_FOLDER/$core_frequency/$uncore_frequency/lic3/$i.csv
+            sudo taskset -c $UNCORE_READER_CORE sudo $IUFD --use-sysfs --measurement-interval 10 --measurement-duration $IUFD_TIMEOUT --start-delta $START_DELTA --stop-delta $STOP_DELTA --outfile $RESULTS_FOLDER/$core_frequency/$uncore_frequency/lic0/uncore-freq-$i.csv &
+            sudo -E $FIRESTARTER -b $BINDLIST --measurement --start-delta=$START_DELTA --start-delta=$STOP_DELTA -t $TIMEOUT -i 6 --run-instruction-groups=REG:100  | tail -n 9 > $RESULTS_FOLDER/$core_frequency/$uncore_frequency/lic0/firestarter-$i.csv
+            wait
+
+            sudo taskset -c $UNCORE_READER_CORE sudo $IUFD --use-sysfs --measurement-interval 10 --measurement-duration $IUFD_TIMEOUT --start-delta $START_DELTA --stop-delta $STOP_DELTA --outfile $RESULTS_FOLDER/$core_frequency/$uncore_frequency/lic1/uncore-freq-$i.csv &
+            sudo -E $FIRESTARTER -b $BINDLIST --measurement --start-delta=$START_DELTA --start-delta=$STOP_DELTA -t $TIMEOUT -i 6 --run-instruction-groups=REG:100,L1_L:100 | tail -n 9 > $RESULTS_FOLDER/$core_frequency/$uncore_frequency/lic1/firestarter-$i.csv
+            wait
+
+            sudo taskset -c $UNCORE_READER_CORE sudo $IUFD --use-sysfs --measurement-interval 10 --measurement-duration $IUFD_TIMEOUT --start-delta $START_DELTA --stop-delta $STOP_DELTA --outfile $RESULTS_FOLDER/$core_frequency/$uncore_frequency/lic2/uncore-freq-$i.csv &
+            sudo -E $FIRESTARTER -b $BINDLIST --measurement --start-delta=$START_DELTA --start-delta=$STOP_DELTA -t $TIMEOUT --run-instruction-groups=REG:100 | tail -n 9 > $RESULTS_FOLDER/$core_frequency/$uncore_frequency/lic2/firestarter-$i.csv
+            wait
+
+            sudo taskset -c $UNCORE_READER_CORE sudo $IUFD --use-sysfs --measurement-interval 10 --measurement-duration $IUFD_TIMEOUT --start-delta $START_DELTA --stop-delta $STOP_DELTA --outfile $RESULTS_FOLDER/$core_frequency/$uncore_frequency/lic3/uncore-freq-$i.csv &
+            sudo -E $FIRESTARTER -b $BINDLIST --measurement --start-delta=$START_DELTA --start-delta=$STOP_DELTA -t $TIMEOUT --run-instruction-groups=L3_L:100 | tail -n 9 > $RESULTS_FOLDER/$core_frequency/$uncore_frequency/lic3/firestarter-$i.csv
+            wait
         done
     done
 done
