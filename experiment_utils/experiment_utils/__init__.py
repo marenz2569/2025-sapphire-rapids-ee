aa2969eb7a54813a237e71c0b0d4419aef7d9774
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-09-02 13:44:44 +0200

    move core-to-core-latency utils to experiment_utils

--- experiment_utils/experiment_utils/__init__.py
+++ experiment_utils/experiment_utils/__init__.py
@@ -3,3 +3,5 @@
 from .experiments import Experiment, ExperimentFilter
 from .isst_perf_profile_parser import TurboRatioLevel, Profile, IsstPerfProfile
 from .plotting import Plotting
+from .lscpu_parser import LscpuInformation
+from .numactl_parser import NumaNode, NumaNodes
--- measurements/tests/core-to-core-latency/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-# pylint: disable=missing-module-docstring, deprecated-module
-
-from .numactl_parser import NumaNode, NumaNodes
-from .lscpu_parser import LscpuInformation
