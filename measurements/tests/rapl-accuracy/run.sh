cf472a77d92017f31a6f946e39ad358a3d6a81d4
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-09-02 18:38:49 +0200

    prepend the elab executable to the path of rapl-accuracy

--- measurements/tests/rapl-accuracy/run.sh
+++ measurements/tests/rapl-accuracy/run.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-# Traverse to the path of this script
-# https://stackoverflow.com/a/24112741
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit ; pwd -P )
-cd "$parent_path" || exit
-
-# Alias elab to execute the elab tool of experiment_utils with root
-# sudo -E is required to have sudo working inside the nix chroot
-alias elab='sudo -E $(which nix) run .#elab --'
-
 echo off | sudo tee /sys/devices/system/cpu/smt/control
 
 export GOMP_CPU_AFFINITY=0-111
