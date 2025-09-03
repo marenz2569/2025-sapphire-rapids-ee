39829602856f59174ec432f030319cd69c0a603a
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-09-03 10:20:56 +0000

    rapl-accuracy: run with root to access the rapl counters

--- measurements/tests/rapl-accuracy/run.sh
+++ measurements/tests/rapl-accuracy/run.sh
@@ -10,4 +10,4 @@ echo "  GOMP_CPU_AFFINITY = $GOMP_CPU_AFFINITY"
 echo "  METRICQ_METRICS   = $METRICQ_METRICS"
 echo "executing test..."
 
-$ROCO2 --csv-output $RESULTS_FOLDER/results.csv
+sudo -E env LD_LIBRARY_PATH=${LD_LIBRARY_PATH} $ROCO2 --csv-output $RESULTS_FOLDER/results.csv
