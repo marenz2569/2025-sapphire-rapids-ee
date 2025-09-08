90675be0dce62635ec9d25fc368686acf3ca91cb
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-09-08 20:38:19 +0200

    measurement/robsize: disable smt

--- measurements/tests/robsize/run.sh
+++ measurements/tests/robsize/run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Take hyperthreads offline
+echo off | sudo tee /sys/devices/system/cpu/smt/control
+
 ulimit -n 999999
 
 $ROBSIZE --stop=600 --outfile=$RESULTS_FOLDER/robsize.csv
