b6db3a30166fbb852e6f4b803b073894d3ed4eaf
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-09-30 21:41:13 +0000

    metricq-power-distribution: save measurement data

--- measurements/tests/metricq-power-distribution/run.py
+++ measurements/tests/metricq-power-distribution/run.py
@@ -3,11 +3,20 @@
 
 # pylint: disable=line-too-long, missing-function-docstring, missing-module-docstring
 
+import pickle
+from pathlib import Path
+import sys
 from metricq import Timestamp, Timedelta
 from datetime import timedelta
 from experiment_utils.metricq import BinnedHistoryClient
 
 def main():
+    if 'RESULTS_FOLDER' not in os.environ:
+        print('RESULTS_FOLDER env variable is not set')
+        sys.exit(1)
+
+    results_foler = Path(os.environ['RESULTS_FOLDER'])
+
     start_timestamp = Timestamp.from_iso8601("2024-01-01T00:00:00.0Z")
     stop_timestamp = Timestamp.from_iso8601("2025-01-01T00:00:00.0Z")
     chunk_size = Timedelta.from_timedelta(timedelta(days=7))
@@ -18,7 +27,8 @@ def main():
     client = BinnedHistoryClient(token="2025-sapphire-rapids-ee", start_timestamp=start_timestamp, stop_timestamp=stop_timestamp, metric_binsize=0.1)
     counters = client.get_counters(metrics=metrics, chunk_size=chunk_size, maximal_aggregation_interval=maximal_aggregation_interval)
 
-    print(counters)
+    with open(results_folder / 'counters.pickle', 'wb') as f:
+        pickle.dump(counters, f)
 
 
 if __name__ == "__main__":
