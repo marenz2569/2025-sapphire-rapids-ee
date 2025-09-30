6f12e75a9fbc7f92a696e8b9a437a97ca15ae295
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-09-30 23:06:34 +0200

    metricq-power-distribution: add stub for measurement

---
+++ measurements/tests/metricq-power-distribution/run.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# pylint: disable=line-too-long, missing-function-docstring, missing-module-docstring
+
+from metricq import Timestamp, Timedelta
+from datetime import timedelta
+from experiment_utils.metricq import BinnedHistoryClient
+
+def main():
+    start_timestamp = Timestamp.from_iso8601("2024-01-01T00:00:00.0Z")
+    stop_timestamp = Timestamp.from_iso8601("2025-01-01T00:00:00.0Z")
+    chunk_size = Timedelta.from_timedelta(timedelta(days=7))
+    maximal_aggregation_interval = Timedelta.from_timedelta(timedelta(seconds=60))
+
+    metrics = [ "barnard.n1001.power" ]
+
+    client = BinnedHistoryClient(token="2025-sapphire-rapids-ee", start_timestamp=start_timestamp, stop_timestamp=stop_timestamp, metric_binsize=0.1)
+    counters = client.get_counters(metrics=metrics, chunk_size=chunk_size,  maximal_aggregation_interval=maximal_aggregation_interval)
+
+    print(counters)
+
+
+if __name__ == "__main__":
+    main()
